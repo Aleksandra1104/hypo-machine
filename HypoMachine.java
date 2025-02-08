be69a5b74b55c53c18e08645a1dec761e07c0697
Sasha11666 <basovasasa087@gmail.com>
2025-02-08 10:36:52 -0500

    debugging

--- HypoMachine.java
+++ HypoMachine.java
@@ -10,6 +10,7 @@ public class HypoMachine {
 
     public static void main(String[] args) {
         initializeSystem();
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter executable file name: ");
         String fileName = scanner.nextLine();
@@ -65,6 +66,7 @@ public class HypoMachine {
                 System.err.println("Error: Invalid PC Address");
                 break;
             }
+            System.out.println("You are in the ExecuteProgram(). Next line of instruction.");
             MAR = PC;
             MBR = memory[(int) MAR];
             IR = MBR;
@@ -74,6 +76,11 @@ public class HypoMachine {
             long Op1GPR = (((IR % 10000) % 1000) / 100);
             long Op2Mode = ((((IR % 10000) % 1000) % 100) / 10);
             long Op2GPR = ((((IR % 10000) % 1000) % 100) % 10);
+            System.out.println("opcode: " + opcode);
+            System.out.println("Op1Mode: " + Op1Mode);
+            System.out.println("Op1GPR: " + Op1GPR);
+            System.out.println("Op2Mode: " + Op2Mode);
+            System.out.println("Op2GPR: " + Op2GPR);
             switch ((int)opcode) {
                 case 0: // Halt
                     isToExecute = false;
@@ -123,15 +130,30 @@ public class HypoMachine {
 
     private static void executeArithmetic( char operator, long Op1Mode, long Op1GPR, long Op2Mode, long Op2GPR ) {
         
-        if(fetchOperand(Op1Mode, Op1GPR)[0] < 0 || fetchOperand(Op2Mode, Op2GPR)[0] < 0) {
+        // if(fetchOperand(Op1Mode, Op1GPR)[1] < 0 || fetchOperand(Op2Mode, Op2GPR)[1] < 0) {
+        //     PSR = -1;
+        //     System.out.println("Cant believe you are herererere");
+        //     return;
+        // }
+        long[] operand1 = new long[2];
+        long[] operand2 = new long[2];
+
+        operand1 = fetchOperand(Op1Mode, Op1GPR);
+        long Op1Value = operand1[1];
+        long Op1Address = operand1[0];
+
+        operand2 = fetchOperand(Op2Mode, Op2GPR);
+        long Op2Value = operand2[1];
+        
+        long result;
+
+        if(Op1Value < 0 || Op2Value < 0) {
             PSR = -1;
+            System.out.println("You are in IF block. Your Op1Value or Op2Value are less than 0");
             return;
         }
 
-        long Op1Value = fetchOperand(Op1Mode, Op1GPR)[1];
-        long Op2Value = fetchOperand(Op2Mode, Op2GPR)[1];
-        long Op1Address = fetchOperand(Op1Mode, Op1GPR)[0];
-        long result;
+        System.out.println("You are in executeArithmetic() after fetching operands and checking that they are > 0");
 
         if(Op2Value == 0 && operator == '/') {
             System.err.println("Error: Division by Zero");
@@ -190,16 +212,16 @@ public class HypoMachine {
     }
 
     private static void executeBranchOnCondition(long Op1Mode, long Op1GPR, java.util.function.LongPredicate condition) {
-        if(fetchOperand(Op1Mode, Op1GPR)[0] < 0) {
-            PSR = -1;
-            return;
-        }
+        
 
         long Op1Value = fetchOperand(Op1Mode, Op1GPR)[1];
-        long Op1Address = fetchOperand(Op1Mode, Op1GPR)[0];
+        // long Op1Address = fetchOperand(Op1Mode, Op1GPR)[0];
+
+        System.out.println("Inside executeBranchOnCondition after fetching Op1Value");
+
     
         long branchAddr = memory[(int) PC];
-        if (Op1Address >= MEMORY_SIZE || branchAddr >= MEMORY_SIZE) {
+        if (branchAddr >= MEMORY_SIZE) {
             System.err.println("Error: Invalid Memory Address");
             PSR = -1;
             return;
@@ -250,6 +272,21 @@ public class HypoMachine {
         System.out.println(message);
         System.out.println("PC: " + PC + " | Clock: " + Clock);
         System.out.println("PSR: " + PSR);
+        System.out.printf("%4s: ", "GPRs");
+        for(int i = 0; i < 8; i++) {
+            System.out.printf("%5s%d ", "GPR", i+1);
+        }
+        System.out.println();
+        System.out.printf("%4s ", " ");
+        for(int i = 0; i < GPRs.length; i++) {
+            System.out.printf("%6d ", GPRs[i]);
+        }
+        System.out.println();
+        System.out.printf("%4s ", " ");
+        for(int i = 0; i < 10; i++ ){
+            System.out.printf("%6s","------");
+        }
+        System.out.println();
         for (int i = start; i < start + size && i < MEMORY_SIZE; i += 10) {
             System.out.printf("%4d: ", i);
             for (int j = 0; j < 10 && (i + j) < MEMORY_SIZE; j++) {
@@ -260,6 +297,7 @@ public class HypoMachine {
     }
 
     private static long[] fetchOperand(long OpMode, long OpGPR) {
+        System.out.println("Hello! You are in fetchOperand()");
         long OpAddress;
         long OpValue;
         long[] operand = new long[2];
@@ -311,18 +349,28 @@ public class HypoMachine {
                 }
                 break;
             case 5: // Direct Mode
+                System.out.println("You are in fetchOperand Mode 5");
+                System.out.println("PC: " + PC);
                 OpAddress = memory[(int)PC++];
+                System.out.println("PC: " + PC);
+                System.out.println("OpMode: " + OpMode );
+                System.out.println("HEEEEY it's OpAddress!" + OpAddress);
                 if(((int) OpAddress) < 10000 && ((int) OpAddress) >= 0 ) {
                     OpValue = memory[(int) OpAddress];
+                    System.out.println("HEEEEY it's OpValue!" + OpValue);
                 } else {
                     System.err.println("Error: Invalid Memory Address");
                     operand[0] = -2;
                     operand[1] = -2;
                     return operand;
                 }
+                System.out.println("You are about to break from switch in FetchOperand");
                 break;
             case 6: // Immediate Mode
+                System.out.println("You are in FetchOperand Immediate Mode 6");
+                System.out.println("PC: " + PC);
                 OpValue = memory[(int) PC++];
+                System.out.println("OpValue: " + OpValue);
                 OpAddress = -1;
                 break;
             default: // Invalid Mode
@@ -334,6 +382,7 @@ public class HypoMachine {
 
         operand[0] = OpAddress;
         operand[1] = OpValue;
+        System.out.println("I am returning operand from fetchOperand()");
         return operand;
     }
 }
